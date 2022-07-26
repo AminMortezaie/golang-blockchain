8ef69a22d63a870dd3164e04dba7ff565824e23c
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-26 18:54:51 +0430

    Feat: Using Badger.

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,10 +1,48 @@
 package blockchain
 
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
 type Blockchain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
 func InitBlockchain() *Blockchain {
+	var lastHash []byte
+
+	opts := badger.DefaultOptions
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No Existing blockchain found.")
+			genesis := Genesis()
+			fmt.Println("Genesis Proved.")
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+			lastHash = genesis.Hash
+			return err
+
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			lastHash, err = item.Value()
+			return err
+		}
+	})
+
 	return &Blockchain{[]*Block{Genesis()}}
 }
 
