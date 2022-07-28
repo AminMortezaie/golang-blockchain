f8c435f4b761a0b142a44e9fbce3d61ec7fa6fa3
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-28 12:42:43 +0430

    Feat: Update.

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -53,3 +53,11 @@ func CoinbaseTx(to, data string) *Transaction {
 func (tx *Transaction) IsCoinbase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
 }
+
+func (in *TxInput) CanUnlock(data string) bool {
+	return in.Sig == data
+}
+
+func (out *TxOutput) CanBeUnlocked(data string) bool {
+	return out.PubKey == data
+}
