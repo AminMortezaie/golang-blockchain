c1f66b3965c0793d6f6e4dfd6d9c4231ffe8f490
aminmortezaie <mohammadmortezaie@gmail.com>
2022-08-01 18:11:00 +0430

    Feat: Update.

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,9 +1,33 @@
 package wallet
 
-import "crypto/ecdsa"
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+
+	"github.com/aminmortezaie/golang-blockchain/blockchain"
+)
 
 //ecdsa stands for Elliptic Curve Digital Signature Algorithm
 type Wallet struct {
 	PrivateKey ecdsa.PrivateKey
 	PublicKey  []byte
+}
+
+func NewKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+
+	//see generate key func which is returning pointer of PrivateKey.
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+
+	blockchain.Handle(err)
+
+	pub := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+	return *private, pub
+}
+
+func MakeWallet() *Wallet {
+	private, public := NewKeyPair()
+	wallet := Wallet{private, public}
+	return &wallet
 }
