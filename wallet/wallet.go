ac6b6d660e664b35c794d5b43165e9de72b76cf8
aminmortezaie <mohammadmortezaie@gmail.com>
2022-08-02 14:39:06 +0430

    Fix: Complete wallets.

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -5,7 +5,6 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
-	"fmt"
 
 	"github.com/aminmortezaie/golang-blockchain/blockchain"
 	"golang.org/x/crypto/ripemd160"
@@ -29,12 +28,8 @@ func (w Wallet) Address() []byte {
 	fullHash := append(versionedHash, checksum...)
 	address := Base58Encode(fullHash)
 
-	fmt.Printf("pub key: %x\n", w.PublicKey)
-	fmt.Printf("pub hash: %x\n", pubHash)
-	fmt.Printf("fullHash: %x\n", fullHash)
-
 	// there is a typo in the original tutorial. %s instead of %x is correct.
-	fmt.Printf("address: %s\n", address)
+	//fmt.Printf("address: %s\n", address)
 	return address
 }
 
--- wallet/wallets.go
+++ wallet/wallets.go
@@ -1,7 +1,78 @@
 package wallet
 
+import (
+	"bytes"
+	"crypto/elliptic"
+	"encoding/gob"
+	"io/ioutil"
+	"os"
+
+	"github.com/aminmortezaie/golang-blockchain/blockchain"
+)
+
 const walletFile = "./tmp/wallets.data"
 
 type Wallets struct {
+	//string would be our address.
 	Wallets map[string]*Wallet
+}
+
+func CreateWallets() (*Wallets, error) {
+	wallets := Wallets{}
+	wallets.Wallets = make(map[string]*Wallet)
+
+	err := wallets.LoadFile()
+	return &wallets, err
+}
+
+func (ws *Wallets) AddWallet() string {
+	wallet := MakeWallet()
+	address := string(wallet.Address())
+	ws.Wallets[address] = wallet
+	return address
+}
+
+func (ws Wallets) GetWallet(address string) Wallet {
+	return *ws.Wallets[address]
+}
+
+func (ws *Wallets) GetAllAddresses() []string {
+	var addresses []string
+
+	for address := range ws.Wallets {
+		addresses = append(addresses, address)
+	}
+	return addresses
+}
+
+func (ws *Wallets) LoadFile() error {
+	if _, err := os.Stat(walletFile); os.IsNotExist(err) {
+		return err
+	}
+
+	var wallets Wallets
+	filecontent, err := ioutil.ReadFile(walletFile)
+
+	blockchain.Handle(err)
+
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(filecontent))
+	err = decoder.Decode(&wallets)
+	blockchain.Handle(err)
+
+	ws.Wallets = wallets.Wallets
+	return nil
+}
+
+func (ws *Wallets) SaveFile() {
+	var content bytes.Buffer
+
+	gob.Register(elliptic.P256())
+
+	encoder := gob.NewEncoder(&content)
+	err := encoder.Encode(ws)
+	blockchain.Handle(err)
+
+	err = ioutil.WriteFile(walletFile, content.Bytes(), 0644)
+	blockchain.Handle(err)
 }
