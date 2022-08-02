9364a52b1430c226efacfb93d4d8fd7c265bbf87
aminmortezaie <mohammadmortezaie@gmail.com>
2022-08-02 11:07:02 +0430

    Feat: Add some encryption into our wallet.

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -4,8 +4,15 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 
 	"github.com/aminmortezaie/golang-blockchain/blockchain"
+	"golang.org/x/crypto/ripemd160"
+)
+
+const (
+	checksumLength = 4
+	version        = byte(0x00)
 )
 
 //ecdsa stands for Elliptic Curve Digital Signature Algorithm
@@ -21,7 +28,7 @@ func NewKeyPair() (ecdsa.PrivateKey, []byte) {
 	private, err := ecdsa.GenerateKey(curve, rand.Reader)
 
 	blockchain.Handle(err)
-
+	// both of private.PublicKey.X and Y are []byte.
 	pub := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
 	return *private, pub
 }
@@ -30,4 +37,23 @@ func MakeWallet() *Wallet {
 	private, public := NewKeyPair()
 	wallet := Wallet{private, public}
 	return &wallet
+}
+
+func PublicKeyHash(pubKey []byte) []byte {
+	pubHash := sha256.Sum256(pubKey)
+
+	hasher := ripemd160.New()
+	_, err := hasher.Write(pubHash[:])
+	blockchain.Handle(err)
+
+	publicRipMD := hasher.Sum(nil)
+	return publicRipMD
+}
+
+// we will hash our payload which is our public key hash two times.
+func Checksum(payload []byte) []byte {
+	firstHash := sha256.Sum256(payload)
+	secondHash := sha256.Sum256(firstHash[:])
+
+	return secondHash[:checksumLength]
 }
